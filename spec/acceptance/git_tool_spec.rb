a1ca134d3f6743025aca2696eeb668a38fc51f67
Riccardo Tacconi <rtacconi@ibahn.com>
2013-05-28 19:38:40 +0200

    delete uneeded specs

--- spec/acceptance/git_tool_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "GIT tool" do
-  context "receives a request to clone a repository" do
-    it "should create a directory with the ID of the project record to avoid name collision"
-    it "should clone the repository the the directory of the project"
-  end
-end
--- spec/views/scripts/index.html.haml_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "scripts/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
