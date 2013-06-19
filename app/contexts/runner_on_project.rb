233d738d987f36d9f75e9dc79c6a8ac9e0cfa7d6
Riccardo Tacconi <rtacconi@ibahn.com>
2013-06-19 10:22:30 +0100

    updated script_name

--- app/contexts/runner_on_project.rb
+++ app/contexts/runner_on_project.rb
@@ -14,9 +14,9 @@ class RunnerOnProject
     # load an ordered script list
     @project.ordered_script_list.each do |script_list|
       # for each script run it
-      executable_script = @project.render_script("#{Rails.root}/app/scripts/#{script_list.script_name}")
+      executable_script = @project.render_script(script_list.script_name)
       # then put pid, exit code and output in script_result
-      script_result = ScriptResult.new(script: script_list.script)
+      script_result = ScriptResult.new(script_name: script_list.script_name)
       result = @project.call_script(executable_script)
       script_result.pid = result[:pid]
       script_result.exit_code = result[:exit_code]
